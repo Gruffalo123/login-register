cd3ba24cb9ca28105492be7d2686f3220e53fd61
unknown <houxinxinbox@163.com>
2019-04-23 19:55:24 +0800

    6

--- login/views.py
+++ login/views.py
@@ -1,15 +1,37 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
 # Create your views here.
-
+from . import models
 
 def index(request):
     pass
     return render(request,'login/index.html')
 
 def login(request):
-    pass
-    return render(request,'login/login.html')
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        #此条message异常
+        message = 'please check your input message'
+        if username.strip() and password:
+            # 用户名字符合法性验证
+            # 密码长度验证
+            # 更多的其它验证.....
+            try:
+                user = models.User.objects.get(name=username)
+            except :
+                message = 'user dose not exist'
+                return render(request, 'login/login.html', {'message': message})
+
+            if user.password == password:
+                print(username, password)
+                return redirect('/index/')
+            else:
+                message = 'wrong password'
+                return render(request, 'login/login.html', {'message': message})
+        else:
+            return render(request, 'login/login.html', {'message': message})
+    return render(request, 'login/login.html')
 
 def register(request):
     pass
